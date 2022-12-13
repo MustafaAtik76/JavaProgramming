63148260febe9ac83bf7d20c73c1794603ec14e9
Mustafa Atik <mustafaatik76@gmail.com>
2022-12-13 17:46:40 +0300

    day38.car BMW and Tesla classes created

---
+++ src/day38_Inheritance/carTask/BMW.java
@@ -0,0 +1,15 @@
+package day38_Inheritance.carTask;
+
+public class BMW extends Car{
+    public BMW( String model, int years, double price, String color, int miles) {
+        super("BMW", model, years, price, color, miles);
+    }
+
+    public void breaksDown(){
+        System.out.println(brand+ " "+ model + " breaks down every 100 miles");
+       }
+
+    public void racing(){
+        System.out.println(brand+ " "+ model+" is a racing car");
+    }
+}
---
+++ src/day38_Inheritance/carTask/Tesla.java
@@ -0,0 +1,12 @@
+package day38_Inheritance.carTask;
+
+public class Tesla extends Car{
+    public Tesla(String model, int years, double price, String color, int miles) {
+        super("Tesla", model, years, price, color, miles);
+    }
+
+    public void autoPilot (){
+
+        System.out.println(brand+ " "+model+" is autopilot mode");
+    }
+}
