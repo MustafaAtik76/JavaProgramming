5d918f6f7412201750e2e28d0bd2e2db4d523581
Mustafa Atik <mustafaatik76@gmail.com>
2022-12-14 14:43:53 +0300

    day39 classes created

--- src/day38_Inheritance/carTask/ParkingLot.java
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -10,6 +10,22 @@ public class ParkingLot {
 
         BMW bmw = new BMW("x5", 2020, 67350, "Black", 12000);
 
+        System.out.println(tesla);
+
+        tesla.start();
+        tesla.autoPilot();
+
+        System.out.println("-----------------------------------------");
+
+        System.out.println(toyota);
+        toyota.start();
+        toyota.reliable();
+
+        System.out.println("--------------------------------------------------");
+
+        System.out.println(bmw);
+        bmw.start();
+        bmw.racing();
 
     }
 }
