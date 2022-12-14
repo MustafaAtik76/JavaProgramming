3e7b311cb24c265e6d7d9cf0e84fcdd02d6a50cf
Mustafa Atik <mustafaatik76@gmail.com>
2022-12-14 15:08:01 +0300

    day39 Square classe created

--- src/day39_Recap/shapeTask/Shape.java
+++ src/day39_Recap/shapeTask/Shape.java
@@ -31,4 +31,24 @@ public class Shape {
         return 0;
     }
 
+    @Override
+    public String toString() {
+        return "Shape{"+
+                "name='" + name + '\''+
+                ", area='" + area() + '\''+
+               ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
 }
+/*Shape:
+	variables:
+			name
+
+	Encapsulate the field
+
+	Add a constructor to set the filed
+
+	Methods:
+		area(){}
+		perimeter(){}
+*/
