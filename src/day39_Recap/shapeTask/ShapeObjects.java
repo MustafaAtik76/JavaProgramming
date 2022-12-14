55683391ba96b5995450432bfd34170d13ee7e37
Mustafa Atik <mustafaatik76@gmail.com>
2022-12-14 15:58:03 +0300

    day39 ShapeObjects classe finished

--- src/day39_Recap/shapeTask/ShapeObjects.java
+++ src/day39_Recap/shapeTask/ShapeObjects.java
@@ -1,4 +1,35 @@
 package day39_Recap.shapeTask;
 
 public class ShapeObjects {
+
+    public static void main(String[] args) {
+
+        Square square = new Square(10.0);
+        System.out.println(square);
+
+        System.out.println(square.getName());
+        System.out.println(square.getSide());
+
+        square.setSide(05);
+        System.out.println(square);
+        System.out.println("----------------------");
+
+        Rectangle rectangle = new Rectangle(5.5, 4.5);
+        System.out.println(rectangle.getName());
+        System.out.println(rectangle);
+
+        rectangle.setWidth(10);
+        rectangle.setLength(12);
+        System.out.println(rectangle);
+        System.out.println("------------------------------");
+
+        Circle circle= new Circle(5.5);
+        System.out.println(circle.getName());
+        System.out.println(circle.getRadius());
+
+        System.out.println(circle);
+
+        circle.setRadius(10);
+        System.out.println(circle);
+    }
 }
